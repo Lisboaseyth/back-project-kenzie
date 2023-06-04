554a54cdac4feea3fddf97b20caf4fb0f8575f95
Pedro Lisboa <lisboa.ti.mkt@gmail.com>
2023-06-04 15:32:25 -0300

    feat: att3

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,12 @@ import { handleErrors } from "./error";
 import { clientRoutes } from "./routes/client.routes";
 import { loginRoutes } from "./routes/login.routes";
 import { contactRoutes } from "./routes/contact.routes";
-import cors from "cors"
 
-// const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger.json'); // Caminho para o arquivo gerado pelo Swagger
+const swaggerDocument = require('../swagger.json');
 
 
 const app: Application = express()
-app.use(cors())
 
 app.use(express.json())
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
@@ -19,8 +16,6 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/client", clientRoutes)
 app.use("/login", loginRoutes)
 app.use("/contact", contactRoutes)
-
-app.get('/', (req, res) => res.json('Hello world'))
 
 app.use(handleErrors)
 
